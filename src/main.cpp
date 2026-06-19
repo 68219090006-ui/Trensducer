ebb379d155baf3ef78cb712d5dbc140a248b3c61
69319090010-oss <68219090006@technictrang.ac.th>
2026-06-19 15:13:36 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <Arduino.h>
 
 const int ledPin = LED_BUILTIN;
+<<<<<<< Updated upstream
 const unsigned long blinkDelayMs = 500;
+=======
+const unsigned long blinkInterval = 500;
+>>>>>>> Stashed changes
 
 void setup() {
   pinMode(ledPin, OUTPUT);
@@ -9,8 +13,15 @@ void setup() {
 
 void loop() {
   digitalWrite(ledPin, HIGH);
+<<<<<<< Updated upstream
   delay(blinkDelayMs);
 
   digitalWrite(ledPin, LOW);
   delay(blinkDelayMs);
+=======
+  delay(blinkInterval);
+
+  digitalWrite(ledPin, LOW);
+  delay(blinkInterval);
+>>>>>>> Stashed changes
 }
